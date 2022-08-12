cd758f806ee880a1d3162015f7ad33c136928a11
Sagar Rajak <sagarrajak@velotios-MacBook-Air.local>
2022-08-12 13:37:08 +0530

    module fix

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodosController } from './todos.controller';
+import { Todo } from './todos.enitity';
+import { TodoService } from './todos.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Todo])],
+  controllers: [TodosController],
+  providers: [TodoService],
+  exports: [TypeOrmModule],
+})
 export class TodosModule {}
